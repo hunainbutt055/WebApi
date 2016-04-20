85e253661cce1787c2af8bb7a297833bb33d2e01
hunainbutt055 <Hunainbutt01@hotmail.com>
2016-04-20 12:22:53 +0500

    First Commit

---
+++ Talent Manager/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Talent_Manager.Models;
+namespace Talent_Manager.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Employee> Get()
+        {
+            return new Employee[] { 
+                new Employee { EmployeeId = 13, Name = "Hunain", FatherName = "Muhammad Zahid butt" },
+               new Employee { EmployeeId = 13, Name = "Hunain", FatherName = "Muhammad Zahid butt" },
+               new Employee {EmployeeId=14,Name="Hunain",FatherName="Muhammad Zahid butt" }
+            };
+        }
+
+        // GET api/values/5
+        public Employee Get(int id)
+        {
+            return new Employee() {EmployeeId=12,Name="Hunain",FatherName="Muhammad Zahid butt" }; 
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Talent Manager/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Talent_Manager.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string FatherName { get; set; }
+    }
+}
